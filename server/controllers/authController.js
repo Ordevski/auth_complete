2c38fb9e704ee5812900e2e4255a727584c783b9
unknown <ordevskidd@gmail.com>
2025-03-04 03:30:57 +0100

    Verification, Reset Password ..

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -158,7 +158,53 @@ export const sendVerifyOtp = async (req, res) => {
 };
 
 export const verifyEmail = async (req, res) => {
+    const { userId, otp } = req.body;
 
+    if(!userId || !otp){
+        return res.json({
+            success: false,
+            message: "Missing details",
+        });
+    }
+    try {
+        const user = await User.findById(userId);
+
+        if (!user) {
+            return res.json({ 
+                success: false, 
+                message: "User not found" 
+            });
+        }
+
+        if(user.verifyOtp === "" || user.verifyOtp !== otp) {
+            return res.json({ 
+                success: false, 
+                message: "Invalid OTP" 
+            });
+        }
+
+        if(user.verifyOtpExpiryTime < Date.now()) {
+            return res.json({ 
+                success: false, 
+                message: "OTP Expired" 
+            });
+        }
+
+        user.isAccountVerified = true;
+        user.verifyOtp = "";
+        user.verifyOtpExpiryTime = 0;
+        await user.save();
+
+        return res.json({ 
+            success: true, 
+            message: "Email verified successfully" 
+        });
+    } catch (error) {
+        res.json({ 
+            success: false, 
+            message: error.message 
+        });
+    }
 };
 
 export const isAuthenticated = async (req, res) => {
@@ -166,9 +212,98 @@ export const isAuthenticated = async (req, res) => {
 };
 
 export const sendResetOtp = async (req, res) => {
+    const { email } = req.body;
 
+    if (!email) {
+        return res.json({
+          success: false,
+          message: "Email is required",
+        });
+    }
+
+    try {
+        const user = await userModel.findOne({ email });
+
+        if (!user) {
+            return res.json({ 
+                success: false, 
+                message: "User not found" 
+            });
+        }
+
+        const otp = String(Math.floor(100000 + Math.random() * 900000));
+
+        user.resetOtp = otp;
+        user.resetOtpExpireAt = Date.now() + 15 * 60 * 1000;
+
+        await user.save();
+
+        const mailOptions = {
+            from: process.env.SENDER_EMAIL,
+            to: user.email,
+            subject: "Password Reset OTP",
+            text: `Your OTP for resetting your password is ${otp}. Use this OTP to proceed with resetting your password.`,
+        };
+
+        await transporter.sendMail(mailOptions);
+        return res.json({ success: true, message: "OTP sent to your email" });
+    } catch (error) {
+        return res.json({ 
+            success: false, 
+            message: error.message 
+        });
+    }
 };
 
 export const resetPassword = async (req, res) => {
+    const { email, otp, newPassword } = req.body;
 
+    if (!email || !otp || !newPassword) {
+        return res.json({
+          success: false,
+          message: "Email, OTP, and New Password are required",
+        });
+    }
+
+    try {
+        const user = await User.findOne({ email });
+
+        if (!user) {
+            return res.json({ 
+                success: false, 
+                message: "User Not Found" 
+            });
+        }
+
+        if(user.resetOtp === "" || user.resetOtp !== otp) {
+            return res.json({ 
+                success: false, 
+                message: "Invalid OTP" 
+            });
+        }
+
+        if (user.resetOtpExpireAt < Date.now()) {
+            return res.json({ 
+                success: false, 
+                message: "OTP Expired" 
+            });
+        }
+
+        const hashedPassword = await bycrypt.hash(newPassword, 10);
+        user.password = hashedPassword;
+        user.resetOtp = "";
+        user.resetOtpExpireAt = 0;
+
+        await user.save();
+
+        return res.json({
+            success: true,
+            message: "Password has been reset successfully",
+        });        
+    } catch (error) {
+        return res.json({ 
+            success: false, 
+            message: error.message 
+        });
+    }
 };
